844975a4ee88e0c08e693ba000299843f489da7d
wnilsen <wnilsen@northinfo.com>
2014-11-12 15:45:50 -0500

    another change to try to get this repo working with readthedocs

--- setup.py
+++ setup.py
@@ -2,18 +2,19 @@ __author__ = 'wnilsen'
 
 from setuptools import setup
 
+long_descr = open('README.md').read()
+
 setup(
     name='python-c3',
     version='0.0.1',
     description="Python wrapper for the C3js D3 plotting library.",
-    long_description="",
+    long_description=long_descr,
     author='Wayne Nilsen',
     author_email='waynenilsen@gmail.com',
     license='MIT',
     packages=['pyc3'],
     zip_safe=False,
     install_requires=[
-        'numpy'
-        , 'pandas'
+        'pandas'
     ]
 )
